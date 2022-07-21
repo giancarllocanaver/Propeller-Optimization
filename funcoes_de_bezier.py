dd145f4b4e3e16d2637105053d740ce6e3783235
giancarllocanaver <saidelcanaver@uol.com.br>
2022-07-21 16:17:58 -0300

    fix(feature/matriz_com_bezier): mudificações fucoes de bezier

--- funcoes_de_bezier.py
+++ funcoes_de_bezier.py
@@ -9,30 +9,144 @@ class Bezier:
 
     Classe com objetivo de execultar diversas funcionalidades de Bezier
     """
-    def __init__(self, aerofolio_base=True, arquivo_base="", linhas_a_pular=None, arquivo=None):
-        self.linhas = None
-        self.A_out = None
-        self.B_out = None
+    def __init__(self, tamanho_entre_pontos_p: int=20, aerofolio_base=True, x=None, y=None):
+        self.tamanho_entre_p = tamanho_entre_pontos_p
+        self.pontos_aerofolio = None
+        self.pontos_a = None
+        self.pontos_b = None
                 
-        if aerofolio_base:
-            x = np.array([1, 0.32957, 0, 0.32957, 1])
-            y = np.array([0, 0.04497, 0, -0.04497, 0])
-
-            self.arquivo_coord_aerof = np.array((x, y))
-
         if not aerofolio_base:
-            self.arquivo_coord_aerof = np.loadtxt(
-                arquivo_base, skiprows=linhas_a_pular
+            self.gerar_aerofolio(
+                x=x,
+                y=y
             )
+        else:
+            self.gerar_aerofolio_base()
 
-            x = arquivo[:, 0]
-            y = arquivo[:, 1]
-            y[0] = 0
-            y[-1] = 0
+    def gerar_aerofolio_base(self):
+        x = np.array([1, 0.32957, 0, 0.32957, 1])
+        y = np.array([0, 0.04497, 0, -0.04497, 0])
 
-            self.arquivo_coord_aerof = np.array((x, y))
+        self.pontos_p = np.array((x, y))
 
-    def gerar_bezier(self, tamanho_entre_pontos, retornar=False):
+    def gerar_aerofolio(self, x: list, y: list):
+        y[0]  = 0
+        y[-1] = 0
+
+        self.pontos_p = np.array((x, y))
+
+    def verificar_interseccao(self, curvas):
+        def intersection(x1, x2, x3, x4, y1, y2, y3, y4):
+            d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+            if d:
+                xs = (
+                    (x1 * y2 - y1 * x2) * (x3 - x4)
+                    - (x1 - x2) * (x3 * y4 - y3 * x4)
+                ) / d
+                ys = (
+                    (x1 * y2 - y1 * x2) * (y3 - y4)
+                    - (y1 - y2) * (x3 * y4 - y3 * x4)
+                ) / d
+                if (
+                    xs >= min(x1, x2)
+                    and xs <= max(x1, x2)
+                    and xs >= min(x3, x4)
+                    and xs <= max(x3, x4)
+                ):
+                    return xs, ys
+
+        xs, ys = [], []
+
+        for i in range(len(curvas[0]) - 1):
+            for j in range(i - 1):
+                if xs_ys := intersection(
+                    curvas[0, i],
+                    curvas[0, i + 1],
+                    curvas[0, j],
+                    curvas[0, j + 1],
+                    curvas[1, i],
+                    curvas[1, i + 1],
+                    curvas[1, j],
+                    curvas[1, j + 1],
+                ):
+                    xs.append(xs_ys[0])
+                    ys.append(xs_ys[1])
+
+        if len(xs) > 1:
+            return 1
+        else:
+            return 0
+    
+    def escalar(self, novas_curvas):
+        """
+        Consegue escalonar um aerofólio entre uma escala de 0 a 1 em x
+
+        input:
+            novas_curvas: todos pontos da curva, numpy array
+                - Linha 0: pontos x
+                - Linha 1: pontos y
+
+        output:
+            new_scale: pontos para o aerofólio normalizado, numpy array
+                - Linha 0: pontos x
+                - Linha 1: pontos y
+        """
+        theta_1 = np.min(novas_curvas[0])
+        theta_2 = np.max(novas_curvas[0])
+
+        x = np.array([])
+        y = np.array([])
+
+        for i in novas_curvas[0]:
+            x_novo = -1 / (theta_1 - theta_2) * (i - theta_2) + 1
+
+            # if x_novo < 1e-05:
+            #     x_novo = 0
+
+            x = np.append(x, x_novo)
+
+        for i in range(len(x)):
+            if x[i] == np.min(x):
+                x[i] = 0
+
+        for i in novas_curvas[1]:
+            y_novo = i / (theta_2 - theta_1)
+
+            y = np.append(y, y_novo)
+
+        new_scale = [x, y]
+
+        return np.array(new_scale)
+
+    def mudar_ponto_A(self, ponto, mudanca_x, mudanca_y, mudanca_adicional=True, retornar=False):
+        """
+        Esta função realiza a mudança nos pontos A de Bezier!
+
+        inputs:
+            ponto: número do ponto a ser modificado (começa em 1)
+
+            mudanca_x: alteração da coordenada em x
+
+            mudanca_y: alteração da coordenada em y
+
+            mudanca_adicional: realiza uma substituição nos pontos A, por definição, False
+
+            retornar: retorna os novos valores de A
+        """
+        a = self.pontos_a.copy()
+        if mudanca_adicional == True:
+            a[0, (ponto - 1)] += mudanca_x
+            a[1, (ponto - 1)] += mudanca_y
+        else:
+            a[0, (ponto - 1)] = mudanca_x
+            a[1, (ponto - 1)] = mudanca_y
+
+        self.pontos_a = a.copy()
+
+        if retornar:
+            return a
+
+    def gerar_pontos_de_bezier(self, retornar=False):
         """
         Gera os coeficientes de Bezier (A,B) e interpola linhas
 
@@ -60,49 +174,7 @@ class Bezier:
                 - Linha 1: ponstos y
         """
 
-        pontos_p = self.arquivo_coord_aerof.copy()
-
-        def interseccao(curvas):
-            def intersection(x1, x2, x3, x4, y1, y2, y3, y4):
-                d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
-                if d:
-                    xs = (
-                        (x1 * y2 - y1 * x2) * (x3 - x4)
-                        - (x1 - x2) * (x3 * y4 - y3 * x4)
-                    ) / d
-                    ys = (
-                        (x1 * y2 - y1 * x2) * (y3 - y4)
-                        - (y1 - y2) * (x3 * y4 - y3 * x4)
-                    ) / d
-                    if (
-                        xs >= min(x1, x2)
-                        and xs <= max(x1, x2)
-                        and xs >= min(x3, x4)
-                        and xs <= max(x3, x4)
-                    ):
-                        return xs, ys
-
-            xs, ys = [], []
-
-            for i in range(len(curvas[0]) - 1):
-                for j in range(i - 1):
-                    if xs_ys := intersection(
-                        curvas[0, i],
-                        curvas[0, i + 1],
-                        curvas[0, j],
-                        curvas[0, j + 1],
-                        curvas[1, i],
-                        curvas[1, i + 1],
-                        curvas[1, j],
-                        curvas[1, j + 1],
-                    ):
-                        xs.append(xs_ys[0])
-                        ys.append(xs_ys[1])
-
-            if len(xs) > 1:
-                return 1
-            else:
-                return 0
+        pontos_p = self.pontos_p.copy()
 
         def coeficientes_de_bezier(pontos_p):
             n = len(pontos_p) - 1
@@ -155,7 +227,7 @@ class Bezier:
             curva_de_bezier = [
                 h(t)
                 for h in curvas
-                for t in np.linspace(0, 1, num=tamanho_entre_pontos)
+                for t in np.linspace(0, 1, num=self.tamanho_entre_p)
             ]
 
             if j == 0:
@@ -174,46 +246,16 @@ class Bezier:
         A_out = np.array((A_out_1, A_out_2))
         B_out = np.array((B_out_1, B_out_2))
 
-        self.linhas = np.reshape(
+        self.pontos_aerofolio = np.reshape(
             linhas, (linhas.shape[0], linhas.shape[1])
         )
-        self.A_out = np.reshape(A_out, (A_out.shape[0], A_out.shape[1]))
-        self.B_out = B_out.copy()
+        self.pontos_a = np.reshape(A_out, (A_out.shape[0], A_out.shape[1]))
+        self.pontos_b = B_out.copy()
 
-        if retornar == True:
-            return linhas, A_out, B_out
+        if retornar:
+            return linhas, A_out, B_out, pontos_p
 
-    def mudar_A(
-        self, ponto, mudanca_x, mudanca_y, mudanca_adicional=True, retornar=False
-    ):
-        """
-        Esta função realiza a mudança nos pontos A de Bezier!
-
-        inputs:
-            ponto: número do ponto a ser modificado (começa em 1)
-
-            mudanca_x: alteração da coordenada em x
-
-            mudanca_y: alteração da coordenada em y
-
-            mudanca_adicional: realiza uma substituição nos pontos A, por definição, False
-
-            retornar: retorna os novos valores de A
-        """
-        a = self.A_out.copy()
-        if mudanca_adicional == True:
-            a[0, (ponto - 1)] += mudanca_x
-            a[1, (ponto - 1)] += mudanca_y
-        else:
-            a[0, (ponto - 1)] = mudanca_x
-            a[1, (ponto - 1)] = mudanca_y
-
-        self.A_out = a.copy()
-
-        if retornar == True:
-            return a
-
-    def bezier_mudar_A(self, tamanho):
+    def mudar_pontos_de_bezier(self):
         """
         Esta definição consegue gerar novas curvas a partir da mudança dos pontos A de Bezier
 
@@ -243,78 +285,8 @@ class Bezier:
                 - Linha 1: pontos y
         """
 
-        pontos = self.arquivo_coord_aerof.copy()
-        a = self.A_out.copy()
-
-        def escala(novas_curvas):
-            theta_1 = np.min(novas_curvas[0])
-            theta_2 = np.max(novas_curvas[0])
-
-            x = np.array([])
-            y = np.array([])
-
-            for i in novas_curvas[0]:
-                x_novo = -1 / (theta_1 - theta_2) * (i - theta_2) + 1
-
-                # if x_novo < 1e-05:
-                #     x_novo = 0
-
-                x = np.append(x, x_novo)
-
-            for i in range(len(x)):
-                if x[i] == np.min(x):
-                    x[i] = 0
-
-            for i in novas_curvas[1]:
-                y_novo = i / (theta_2 - theta_1)
-
-                y = np.append(y, y_novo)
-
-            new_scale = [x, y]
-
-            return np.array(new_scale)
-
-        def interseccao(curvas):
-            def intersection(x1, x2, x3, x4, y1, y2, y3, y4):
-                d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
-                if d:
-                    xs = (
-                        (x1 * y2 - y1 * x2) * (x3 - x4)
-                        - (x1 - x2) * (x3 * y4 - y3 * x4)
-                    ) / d
-                    ys = (
-                        (x1 * y2 - y1 * x2) * (y3 - y4)
-                        - (y1 - y2) * (x3 * y4 - y3 * x4)
-                    ) / d
-                    if (
-                        xs >= min(x1, x2)
-                        and xs <= max(x1, x2)
-                        and xs >= min(x3, x4)
-                        and xs <= max(x3, x4)
-                    ):
-                        return xs, ys
-
-            xs, ys = [], []
-
-            for i in range(0, (len(curvas[0]) - 1), 1):
-                for j in range(0, (i - 1), 1):
-                    if xs_ys := intersection(
-                        curvas[0, i],
-                        curvas[0, i + 1],
-                        curvas[0, j],
-                        curvas[0, j + 1],
-                        curvas[1, i],
-                        curvas[1, i + 1],
-                        curvas[1, j],
-                        curvas[1, j + 1],
-                    ):
-                        xs.append(xs_ys[0])
-                        ys.append(xs_ys[1])
-
-            if len(xs) > 1:
-                return 1
-            else:
-                return 0
+        pontos = self.pontos_p.copy()
+        a = self.pontos_a.copy()
 
         def novos_pontos_p(a, pontos, n):
             p_1 = (2 * a[0] + a[1] - pontos[0]) / 2
@@ -326,21 +298,15 @@ class Bezier:
             pontos_intermediarios = np.append(pontos_intermediarios, pontos[0])
             pontos_intermediarios = np.append(pontos_intermediarios, p_1)
 
-            # Pontos_intermediarios.append(pontos[0])
-            # Pontos_intermediarios.append(P_1)
-
             for i in range(1, n - 2):
                 p_i_mais_1 = (
                     a[i - 1] + 4 * a[i] + a[i + 1]
                 ) / 2 - 2 * pontos_intermediarios[i]
 
                 pontos_intermediarios = np.append(pontos_intermediarios, p_i_mais_1)
-                # pontos_intermediarios.append(p_i_mais_1)
 
             pontos_intermediarios = np.append(pontos_intermediarios, p_n_menos_1)
             pontos_intermediarios = np.append(pontos_intermediarios, pontos[-1])
-            # Pontos_intermediarios.append(P_n_menos_1)
-            # Pontos_intermediarios.append(pontos[-1])
 
             return pontos_intermediarios
 
@@ -377,7 +343,7 @@ class Bezier:
             ]
 
             curva_de_bezier = [
-                h(t) for h in curvas for t in np.linspace(0, 1, num=tamanho)
+                h(t) for h in curvas for t in np.linspace(0, 1, num=self.tamanho_entre_p)
             ]
             if j == 0:
                 linhas_1 = curva_de_bezier
@@ -409,53 +375,35 @@ class Bezier:
 
         linhas = np.array((array_final_um, array_final_dois))
 
-        self.linhas = linhas.copy()
-        self.A_out = a.copy()
-        self.B_out = B_out.copy()
+        self.pontos_aerofolio = linhas.copy()
+        self.pontos_a = a.copy()
+        self.pontos_b = B_out.copy()
 
-        if interseccao(linhas) == 1:
+        if self.verificar_interseccao(linhas):
             return np.array([[0], [0]]), 0, 0, 0
         else:
-            linhas = escala(linhas)
+            linhas = self.escalar(linhas)
             return linhas, a, B_out, novos_pontos
 
-    def escala(novas_curvas):
-        """
-        Consegue escalonar um aerofólio entre uma escala de 0 a 1 em x
+    def atualizar_aerofolio(self, pontos_x, pontos_y):
+        pontos = []
+        for i in range(4):
+            pontos.append([pontos_x[i][0], pontos_y[i][0]])
 
-        input:
-            novas_curvas: todos pontos da curva, numpy array
-                - Linha 0: pontos x
-                - Linha 1: pontos y
+        ponto = 0
+        for ponto_x, ponto_y in pontos:
+            ponto = ponto + 1
+            self.mudar_ponto_A(
+                ponto=ponto,
+                mudanca_x=ponto_x,
+                mudanca_y=ponto_y,
+                mudanca_adicional=False
+            )
 
-        output:
-            new_scale: pontos para o aerofólio normalizado, numpy array
-                - Linha 0: pontos x
-                - Linha 1: pontos y
-        """
-        theta_1 = np.min(novas_curvas[0])
-        theta_2 = np.max(novas_curvas[0])
+        curvas_aerofolio, a, b, pontos_p = self.mudar_pontos_de_bezier()
+        
+        self.pontos_a = a.copy()
+        self.pontos_b = b.copy()
+        self.pontos_p = pontos_p.copy()
 
-        x = np.array([])
-        y = np.array([])
-
-        for i in novas_curvas[0]:
-            x_novo = -1 / (theta_1 - theta_2) * (i - theta_2) + 1
-
-            # if x_novo < 1e-05:
-            #     x_novo = 0
-
-            x = np.append(x, x_novo)
-
-        for i in range(len(x)):
-            if x[i] == np.min(x):
-                x[i] = 0
-
-        for i in novas_curvas[1]:
-            y_novo = i / (theta_2 - theta_1)
-
-            y = np.append(y, y_novo)
-
-        new_scale = [x, y]
-
-        return np.array(new_scale)
+        return curvas_aerofolio, a, b, pontos_p
