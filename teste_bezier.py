8860ebc9a16ec6992c4d7aac3af284ac2c3af328
giancarllocanaver <saidelcanaver@uol.com.br>
2022-07-22 16:23:57 -0300

    build(feature/matriz_com_bezier): adição de testes

---
+++ teste_bezier.py
@@ -0,0 +1,33 @@
+from funcoes_de_bezier import Bezier
+from matplotlib import pyplot as plt
+import numpy as np
+
+bezier_controler = Bezier(
+    aerofolio_base=True
+)
+linhas, a0, b0, pontos_p = bezier_controler.gerar_pontos_de_bezier(retornar=True)
+
+novos_A = a0.copy()
+novos_A[0,0] = novos_A[0,0] - 0.05
+novos_A[0,1] = novos_A[0,1] - 0.05
+novos_A[0,2] = novos_A[0,2] + 0.05
+novos_A[0,3] = novos_A[0,3] - 0.05
+
+novos_A[1,0] = novos_A[1,0] - 0.005
+novos_A[1,1] = novos_A[1,1] + 0.005
+novos_A[1,2] = novos_A[1,2] - 0.005
+novos_A[1,3] = novos_A[1,3] + 0.005
+
+novas_linhas, a1, b1, novos_pontos_p = bezier_controler.atualizar_aerofolio(
+    pontos_x=novos_A[0],
+    pontos_y=novos_A[1]
+)
+
+
+plt.plot(linhas[0], linhas[1])
+plt.plot(novas_linhas[0], novas_linhas[1])
+plt.plot(a0[0], a0[1], 'x')
+plt.plot(a1[0], a1[1], 'x')
+plt.grid()
+plt.ylim((-0.1,0.1))
+plt.show()
---
+++ teste_pso.py
@@ -0,0 +1,7 @@
+from PSO import OtimizacaoHelice
+
+a = OtimizacaoHelice(
+    qde_iteracoes=10,
+    qde_de_particulas=10,
+    otimizar=['bezier', 'beta']
+)
