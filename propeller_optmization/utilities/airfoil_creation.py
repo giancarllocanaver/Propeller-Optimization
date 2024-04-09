5e531d339f0d1065024f515015c08e79330e2821
giancarllocanaver <saidelcanaver@uol.com.br>
2024-04-09 11:16:05 -0300

    refat(feature): refatoração módulo de criação de aerofólios

--- propeller_optmization/utilities/airfoil_creation.py
+++ propeller_optmization/utilities/airfoil_creation.py
@@ -1,9 +1,12 @@
 import subprocess
 import time
+import os
 
 import numpy as np
 
-from propeller_optmization.data_structures import PPoints
+# from propeller_optmization.data_structures import PPoints
+from uuid import uuid4
+
 
 class AirfoilCreation:
     def __init__(self) -> None:
@@ -12,23 +15,30 @@ class AirfoilCreation:
 
     def generate_airfoil(self, airfoil_name: str) -> None:
         self.airfoil_name = airfoil_name
-        self.airfoil_dir = f'processing/execution_steps/{airfoil_name}.txt'
+        self.airfoil_dir = os.path.join(
+            os.getcwd(),
+            "processing",
+            "execution_steps",
+            airfoil_name + ".txt",
+        )
 
-        self._create_airfoil()
-    
+        self.__create_airfoil()
+
     def obtain_p_points(self):
-        points = np.loadtxt(self.airfoil_dir, skiprows=1)        
-        points_p = np.array([points[:,0], points[:,1]])
+        points = np.loadtxt(self.airfoil_dir, skiprows=1)
+        points_p = np.array([points[:, 0], points[:, 1]])
 
-        points_p[1][0]  = 0
+        points_p[1][0] = 0
         points_p[1][-1] = 0
 
         return points_p
 
-    def _create_airfoil(self):
-        airfoil_generation_dir_file = 'processing/execution_steps/generation_airfoil_file.txt'
-        
-        with open(airfoil_generation_dir_file, 'w') as writer:
+    def __create_airfoil(self):
+        airfoil_generation_dir_file = (
+            "processing/execution_steps/generation_airfoil_file.txt"
+        )
+
+        with open(airfoil_generation_dir_file, "w") as writer:
             writer.write("PLOP\n")
             writer.write("G\n")
             writer.write(f"\n")
@@ -40,15 +50,36 @@ class AirfoilCreation:
             writer.write(f"\n")
             writer.write(f"\n")
             writer.write(f"save\n")
-            writer.write(f"{self.airfoil_dir}\n")
+            writer.write(f"processing/execution_steps/{self.airfoil_name}.txt\n")
             writer.write(f"\n")
             writer.write(f"quit\n\n")
 
+        exe_dir = os.path.join(
+            "processing",
+            "xfoil_instances",
+            "xfoil.exe",
+        )
+
         subprocess.Popen(
-            "xfoil.exe < " + airfoil_generation_dir_file, shell=True
+            exe_dir + " < " + airfoil_generation_dir_file,
+            shell=True,
         )
         time.sleep(0.5)
 
     @staticmethod
-    def create_airfoil_in_xfoil_from_splines() -> str:
-        pass
+    def create_airfoil_in_xfoil_from_splines(spline: np.ndarray) -> str:
+        spline = spline.reshape((spline.shape[0], spline.shape[1]))
+        x_points = spline[0]
+        y_points = spline[1]
+
+        uuid_airfoil = str(uuid4())[:10].replace("-", "")
+        filename_dir = f"processing/execution_steps/airfoil_{uuid_airfoil}.dat"
+
+        with open(filename_dir, "w") as writer:
+            for point in range(len(x_points)):
+                writer.write(
+                    f"{round(x_points[point], 4)}    {round(y_points[point], 4)}\n"
+                )
+            writer.close()
+
+        return filename_dir
