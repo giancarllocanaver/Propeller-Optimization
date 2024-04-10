5f28b34e4703422af3c4b6d434511508cec1147a
giancarllocanaver <saidelcanaver@uol.com.br>
2024-04-10 06:55:33 -0300

    build(feature): inserção de métodos para TEP

--- propeller_optmization/blade_element_theory.py
+++ propeller_optmization/blade_element_theory.py
@@ -28,6 +28,9 @@ class BladeElementTheory:
         self.__calculate_resultant_velocity()
         self.__calculate_reynolds_and_mach()
         self.__calculate_Cl_and_Cd()
+        self.__calculate_gamma()
+        self.__calculate_dt_and_dq()
+        self.__calculate_efficiency()
 
         return self.results
 
@@ -115,6 +118,83 @@ class BladeElementTheory:
             results += results_t
 
         self.cl_cd_results = results
+
+    def __calculate_gamma(self):
+        self.gamma = [
+            np.arctan(sec[2] / sec[1]) if sec[1] != 0 else None
+            for sec in self.cl_cd_results
+        ]
+
+    def __calculate_dt_and_dq(self):
+        q = (
+            0.5
+            * self.flight_conditions.get("airDensity")
+            * self.flight_conditions.get("speed") ** 2
+        )
+
+        den = lambda secao: np.cos(self.gamma[secao]) * np.sin(self.phi[secao]) ** 2
+
+        self.dt = [
+            (
+                (
+                    q
+                    * sec[1]
+                    * self.propeller_geometric_conditions.get("chord")[sec[0]]
+                    * np.cos(self.phi[sec[0]] + self.gamma[sec[0]])
+                    / den(sec[0])
+                )
+                if self.gamma[sec[0]] is not None
+                else 0
+            )
+            for sec in self.cl_cd_results
+        ]
+        self.dt.append(0)
+
+        self.dq = [
+            (
+                (
+                    q
+                    * sec[1]
+                    * self.propeller_geometric_conditions.get("chord")[sec[0]]
+                    * self.propeller_geometric_conditions.get("radius")[sec[0]]
+                    * np.sin(self.phi[sec[0]] + self.gamma[sec[0]])
+                    / den(sec[0])
+                )
+                if self.gamma[sec[0]] is not None
+                else 0
+            )
+            for sec in self.cl_cd_results
+        ]
+        self.dq.append(0)
+
+    def __calculate_efficiency(self):
+        def integrate(y: list, x: list):
+            i = 0
+            for n in range(1, len(x)):
+                i += (x[n] - x[n - 1]) * (y[n] + y[n - 1]) / 2
+
+            return i
+
+        traction = integrate(self.dt, self.propeller_geometric_conditions.get("radius"))
+        torque = integrate(self.dq, self.propeller_geometric_conditions.get("radius"))
+
+        n = self.flight_conditions.get("engineSpin") / 60
+
+        t_coeffic = traction / (
+            self.flight_conditions.get("airDensity")
+            * n**2
+            * self.propeller_geometric_conditions.get("bladeDiameter") ** 4
+        )
+        q_coeffic = torque / (
+            self.flight_conditions.get("airDensity")
+            * n**2
+            * self.propeller_geometric_conditions.get("bladeDiameter") ** 5
+        )
+        p_coeffic = 2 * np.pi * q_coeffic
+
+        self.efficiency = (
+            self.advance_rate * t_coeffic / p_coeffic if p_coeffic != 0 else None
+        )
 
 
 def execute_xfoil(
